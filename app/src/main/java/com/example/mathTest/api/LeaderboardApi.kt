5cbfd55c6d20b6f2f126e0d2a40ae7a4d5b3a8e3
Sarthak Gandhi <sarthakgandhi1692@gmail.com>
2025-06-15 20:51:38 +0530

    updated leaderboard api

--- app/src/main/java/com/example/mathTest/api/LeaderboardApi.kt
+++ app/src/main/java/com/example/mathTest/api/LeaderboardApi.kt
@@ -5,6 +5,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface LeaderboardApi {
-    @GET("api/test/game/leaderboard")
+    @GET("api/leaderboard")
     suspend fun getLeaderboard(): Response<LeaderboardResponse>
 } 
